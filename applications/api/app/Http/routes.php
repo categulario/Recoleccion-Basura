85419ca4410b7824fcf461d23988af329cf2c9b0
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2016-11-22 15:43:03 -0600

    this is how I think that this API should be

--- applications/api/app/Http/routes.php
+++ applications/api/app/Http/routes.php
@@ -2,19 +2,6 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-
-
-/*
-|--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
@@ -26,34 +13,35 @@
 
 Route::group(['middleware' => ['web','cors']], function () {
 	Route::get('/', function () {
-    return view('index');
+		return view('index');
 	});
-//API v1
-Route::group(['prefix' => 'v1'], function () {
-	Route::get('gruposVehiculo/', 'GruposVehiculoController@index');
-	
-	Route::get('gruposVehiculo/{grupoId}/ultimasPosicionesVehiculos', 
-		'UltimasPosicionesVehiculosController@index')->where(array('grupoId' => '[a-zA-Z0-9\-]+'));
-	
-	Route::get('gruposVehiculo/{grupoId}/vehiculos',
-		'VehiculosController@index')->where(array('grupoId' => '[a-zA-Z0-9\-]+'));
 
-	Route::get('vehiculos/{vehiculoId}/ultimaPosicionVehiculo',
-		'UltimaPosicionVehiculoController@index')->where(array('vehiculoId' => '[a-zA-Z0-9\-]+'));
-	
-	Route::get('vehiculos/{vehiculoId}/viajes', 
-		'ViajesController@index')->where(array('vehiculoId' => '[a-zA-Z0-9\-]+'));
+	//API v1
+	Route::group(['prefix' => 'v1'], function () {
+		Route::get('group/', 'GruposVehiculoController@index');
 
-	Route::get('viajes/{viajeId}/posicionesViaje', 
-		'PosicionesViajeController@index')->where(array('viajeId' => '[0-9]+'));
+		Route::get('group/{grupoId}/position', 
+			'UltimasPosicionesVehiculosController@index')->where(array('grupoId' => '[a-zA-Z0-9\-]+'));
 
-	Route::get('vehiculos/{vehiculoId}/viajes/resumen', 
-		'ResumenController@resumenVehiculo')->where(array('vehiculoId' => '[a-zA-Z0-9\-]+'));
+		Route::get('group/{grupoId}/bus',
+			'VehiculosController@index')->where(array('grupoId' => '[a-zA-Z0-9\-]+'));
 
-	Route::get('gruposVehiculo/{grupoId}/viajes/resumen',
-		'ResumenController@index')->where(array('grupoId' => '[a-zA-Z0-9\-]+'));
+		Route::get('group/{grupoId}/track/summary',
+			'ResumenController@index')->where(array('grupoId' => '[a-zA-Z0-9\-]+'));
 
-	Route::get('sitios', 'SitiosController@index');   
+		Route::get('bus/{vehiculoId}/position',
+			'UltimaPosicionVehiculoController@index')->where(array('vehiculoId' => '[a-zA-Z0-9\-]+'));
+
+		Route::get('bus/{vehiculoId}/track', 
+			'ViajesController@index')->where(array('vehiculoId' => '[a-zA-Z0-9\-]+'));
+
+		Route::get('bus/{vehiculoId}/track/summary', 
+			'ResumenController@resumenVehiculo')->where(array('vehiculoId' => '[a-zA-Z0-9\-]+'));
+
+		Route::get('track/{viajeId}/location', 
+			'PosicionesViajeController@index')->where(array('viajeId' => '[0-9]+'));
+
+		Route::get('place', 'SitiosController@index');   
 	});
 });
 
